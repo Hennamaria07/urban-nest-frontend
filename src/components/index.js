b972c31fb8e88ff42862a0bda93efdb7d6c89022
Henna Maria <hennamaria2001@gmail.com>
2024-06-19 14:13:20 +0530

    reusable card loader created

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Login from "./auth component/Login";
 import Signup from "./auth component/Signup";
+import CardLoader from "./user components/CardLoader";
 import ProductCard from "./user components/ProductCard";
 
 export {
     Login,
     Signup,
-    ProductCard
+    ProductCard,
+    CardLoader
 }
