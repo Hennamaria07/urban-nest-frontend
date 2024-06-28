85891c6bf4b6ee7ed02cc0e81d3e55548ad9e435
Henna Maria <hennamaria2001@gmail.com>
2024-06-28 09:55:37 +0530

    vercel path added

--- src/lib/axiosInstance.jsx
+++ src/lib/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://ecommerce-server-voxn.onrender.com"
+    // baseURL: "https://ecommerce-server-voxn.onrender.com"
     // baseURL: "http://localhost:4000"
-    // baseURL: "https://urban-nest-8h06skkkq-henna-marias-projects.vercel.app"
+    baseURL: "https://urban-nest-8h06skkkq-henna-marias-projects.vercel.app"
 })
